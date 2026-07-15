23ff5f1d45e18e4bcd6c0acac2a215afdb58467f
Karthikeya <karthikeyapranav2@gmail.com>
2026-07-15 21:54:48 +0530

    x^n

---
+++ xPowerN.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class xPowerN {
+    public static void Recur(int x, int n, int r){
+        if(n == 0){
+            System.out.print(r);
+            return;
+        }
+        r = r * x; //we cant put r = 0.
+        n--;
+        Recur(x, n, r);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the base number");
+        int x = sc.nextInt();
+        System.out.println("Enter the power");
+        int n = sc.nextInt();
+        int r = 1;
+        Recur(x, n, r);
+        sc.close();
+    }
+}
