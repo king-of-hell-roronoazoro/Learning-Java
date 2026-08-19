c643cee361e1e837ef582b1e0a1ff211c234aea4
Karthikeya Pranav <karthikeyapranav2@gmail.com>
2026-08-19 19:30:15 +0530

    some modifications to OOP1

--- OOP.java
+++ OOP.java
@@ -1,6 +1,10 @@
 class employee{
     int id;
     String name;
+    public void printDetails(){
+        System.out.println("name is: " + name);
+        System.out.println("id is: " + id);
+    }
 }
 public class OOP{
     public static void main(String args[]){
@@ -9,9 +13,11 @@ public class OOP{
         admin.name="karthikeya";
         admin.id=6769;
         //but if i type admin.age it wont work coz we have to define age in the class employee
-        System.out.println(admin.name);
+        //System.out.println(admin.name);
         employee noob = new employee();
         noob.name="vamsi";
-        System.out.println("noob name is: " + noob.name);
+        //System.out.println("noob name is: " + noob.name);
+        admin.printDetails(); //calling the method printDetails() of employee class,so that i dont have to sout very time.
+        noob.printDetails();
     }
 }
