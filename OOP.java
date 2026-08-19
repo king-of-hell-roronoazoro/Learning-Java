545f70011ea4e9d4d942894277401ee0ca15b0ec
Karthikeya Pranav <karthikeyapranav2@gmail.com>
2026-08-19 18:54:17 +0530

    OOP

---
+++ OOP.java
@@ -0,0 +1,17 @@
+class employee{
+    int id;
+    String name;
+}
+public class OOP{
+    public static void main(String args[]){
+        System.out.println("Creating an object of employee");
+        employee admin = new employee();
+        admin.name="karthikeya";
+        admin.id=6769;
+        //but if i type admin.age it wont work coz we have to define age in the class employee
+        System.out.println(admin.name);
+        employee noob = new employee();
+        noob.name="vamsi";
+        System.out.println("noob name is: " + noob.name);
+    }
+}
